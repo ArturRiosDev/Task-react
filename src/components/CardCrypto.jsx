6905fdb594c2f7016d3a5c361fa62a52978d0d23
Arturo Rios <arturo_rios@epam.com>
2022-06-18 08:15:24 -0500

    finish task

--- src/components/CardCrypto.jsx
+++ src/components/CardCrypto.jsx
@@ -1,9 +1,18 @@
-export default function Label({cryptoList}) {
+export default function Label({cryptoList, filteredCoins}) {
     return (
         <div id="app">
             <section className="Items">
-              {
+              {cryptoList && !filteredCoins &&
                 cryptoList.map(({image, name, current_price},index)=>{
+                  return (
+                    <article key={index}>
+                       <img src={image} alt='Crypto' /><h3>${name}</h3><h1>${current_price}</h1>
+                    </article>
+                  )
+                })
+              }
+              {filteredCoins &&
+                filteredCoins.map(({image, name, current_price},index)=>{
                   return (
                     <article key={index}>
                        <img src={image} alt='Crypto' /><h3>${name}</h3><h1>${current_price}</h1>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import api from '../lib/api';
 import {nextPage} from '../funct/functions'
 import CardCrypto from './CardCrypto'
 
-export default function Label() {
+export default function Title() {
 
     useEffect(()=>{
         api.getData().then(res=>{
@@ -12,6 +12,7 @@ export default function Label() {
     },[])
 
     const [cryptoList,setCryptoList] = useState([])
+    const [filteredCoin,setFilteredCoin] = useState(null)
 
     const loadData = ()=>{
         if(nextPage(cryptoList)){
@@ -20,12 +21,10 @@ export default function Label() {
         api.getData().then(res => setCryptoList(res.slice(0, cryptoList.length + 5)))
        }
 
-    let sumMarket = cryptoList.reduce((i,val)=>i+val.current_price,0)
-
     const filterData = e =>{
         let input = e.target.value
-        let updated = cryptoList.filter(({name})=> name.includes(input))
-        setCryptoList(updated)
+        let updated = cryptoList.filter(({name})=> name.toLowerCase().includes(input.toLowerCase()))
+        !input? setFilteredCoin(null): setFilteredCoin(updated)
       }
 
   return (
@@ -37,14 +36,16 @@ export default function Label() {
       <div className="Form">
         <input onChange={filterData} id="filter" />
         <label id="results">
-          Results: <span>{cryptoList.length}</span>
+          Results: <span>{!filteredCoin ? cryptoList.length : filteredCoin.length}</span>
         </label>
         <label id="market_cap">
-          Sum Market Cap: $ <span>{sumMarket}</span>
+          Sum Market Cap: $ <span>{!filteredCoin? cryptoList.reduce((i,val)=>i+val.current_price,0): filteredCoin.reduce((i,val)=>i+val.current_price,0)
+          }</span>
         </label>
       </div>
       <CardCrypto
         cryptoList={cryptoList}
+        filteredCoins={filteredCoin}
       />
     </>
   );
